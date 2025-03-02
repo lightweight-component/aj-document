891822d239dad9a808adc0444791b94ca03f3a84
sp42 <sp42@qq.com>
2025-03-02 15:35:27 +0800

    ci

---
+++ aj-database-doc/src/test/java/com/ajaxjs/database_meta/BaseTestConfig.java
@@ -0,0 +1,9 @@
+package com.ajaxjs.database_meta;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ajaxjs.database_meta")
+public class BaseTestConfig {
+}
